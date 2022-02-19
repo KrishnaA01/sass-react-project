ee2f1433db4e9c37124154209d0256e5fe69ad12
KrishnaA01 <mahato77@hotmail.com>
2022-02-19 13:38:38 +0100

    hero banner

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.scss'
 import React from 'react';
 import Header from './components/Header';
+import { Section } from './components/Section';
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <Section/>
     </div>
   );
 }
